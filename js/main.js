acd884fa33c1bc338e772d4d0a1beb9da3eefdf4
Daniel Carlyle <dcarlyle@rto-ero.org>
2020-01-27 11:08:09 -0500

    latest sponsor additions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 	$("#portfolio-contant-active").mixItUp();
 
 
 	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
+		paginationSpeed: 500,
+		singleItem: true,
+		autoPlay: 3000,
 	});
 
 
@@ -15,35 +15,53 @@ $(document).ready(function(){
 
 	$("#clients-logo").owlCarousel({
 		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
+		items: 5,
+		itemsDesktop: [1199, 5],
+		itemsDesktopSmall: [979, 5],
 	});
 
 	$("#works-logo").owlCarousel({
 		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
+		items: 5,
+		itemsDesktop: [1199, 5],
+		itemsDesktopSmall: [979, 5],
 	});
 
 
 	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: { lat: -34.397, lng: 150.644 },
+			zoom: 8
+		});
+	}
 
 
 	// Counter
 
 	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+		delay: 10,
+		time: 1000
+	});
+
+
+
+	// ===== Scroll to Top ==== 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+			$('#return-to-top').fadeIn(200);    // Fade in the arrow
+		} else {
+			$('#return-to-top').fadeOut(200);   // Else fade out the arrow
+		}
+	});
+	$('#return-to-top').click(function () {      // When arrow is clicked
+		$('body,html').animate({
+			scrollTop: 0                       // Scroll to top of body
+		}, 500);
+	});
+
+
 
 
 });
